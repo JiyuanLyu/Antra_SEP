41551d52ea449523bfd0b6a5e096aff71d6f1ac9
JiyuanLyu <JiyuanLyu>
2022-08-21 22:58:40 +0000

    create

---
+++ Raw to Bronze Notebook.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Raw to Bronze Notebook
+# MAGIC In this notebook, I turned 8 movie JSON files into a single bronze delta table.
+
+# COMMAND ----------
+
+
